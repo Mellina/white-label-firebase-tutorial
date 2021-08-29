8b9ffe89e9b5cf6600d8f70c1ea46d7f30cdd0a6
Mellina <mellinacerqueira@gmail.com>
2021-08-28 23:25:26 -0300

    implementacao dos use cases

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/CreateProductUseCase.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/CreateProductUseCase.kt
@@ -1,8 +1,9 @@
 package br.com.douglasmotta.whitelabeltutorial.domain.usecase
 
 import android.net.Uri
+import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 
 interface CreateProductUseCase {
 
-    suspend operator fun invoke(description: String, price: Double, imageUri: Uri): String
+    suspend operator fun invoke(description: String, price: Double, imageUri: Uri): Product
 }
